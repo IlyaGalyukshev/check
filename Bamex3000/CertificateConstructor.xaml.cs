f9baf922d237adb02195ab76ec5a19214b59b6be
IlyaGalyukshev <86611426+IlyaGalyukshev@users.noreply.github.com>
2021-07-01 02:11:57 +0600

    Add files via upload

---
+++ Bamex3000/CertificateConstructor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bamex3000
+{
+    public partial class CertificateConstructor : Window
+    {
+        double startPosX, startPosY, finPosX, finPosY;
+
+        public CertificateConstructor()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
+            //this.Background = new ImageBrush(new BitmapImage(new Uri(@"C:\Users\theho\Desktop\bamex3000\Bamex3000\bin\Debug\inew.jpg")));
+            //получить содержимое переменной bgPath из файла MainWindow.xaml.cs
+        }
+
+        private void WndConstruct_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbName.Visibility = Visibility.Hidden;
+            startPosX = Mouse.GetPosition(wndConstruct).X;
+            startPosY = Mouse.GetPosition(wndConstruct).Y;
+        }
+
+        private void WndConstruct_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            finPosX = Mouse.GetPosition(wndConstruct).X;
+            finPosY = Mouse.GetPosition(wndConstruct).Y;
+            tbName.Margin = new Thickness(Math.Min(startPosX, finPosX), Math.Min(startPosY, finPosY), 0, 0);
+            tbName.Width = Math.Abs(finPosX - startPosX);
+            tbName.Height = Math.Abs(finPosY - startPosY);
+            tbName.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ Bamex3000/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace Bamex3000
+{
+    public partial class MainWindow : Window
+    {
+        public string bgPath, dbPath, finalPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenConstructor_Click(object sender, RoutedEventArgs e)
+        {
+            CertificateConstructor window = new CertificateConstructor();
+            window.Height = 700;
+            window.Width = 700 / Math.Sqrt(2);
+            window.Show();
+        }
+
+        private void BtnOpenBG_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() == true)
+            {
+                bgPath = dlg.FileName;
+            }
+        }
+
+        private void BtnOpenDataBase_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() == true)
+            {
+                dbPath = dlg.FileName;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            if (dlg.ShowDialog() == true) {
+                finalPath = dlg.FileName;
+                /*реализация сохранения готовых сертификатов*/
+            }
+        }
+    }
+}
